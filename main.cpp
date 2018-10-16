270561fd672fb8081479d4a0dc83fd79412dfd10
ajmoebius <ajmoebius@o244-10.csuchico.edu>
2018-10-15 17:07:02 -0700

    functional

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,27 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+  if(argc == 2 )
+  {
+    string filename = argv[1];
+    vector<vector<Pixel> >imagePixels; 
+
+    Bitmap image;
+    image.open(filename);
+    if(image.isImage())
+    {
+      imagePixels = image.toPixelMatrix();
+      cout<<filename<<" is "<<imagePixels.size()<<" high and "
+        <<imagePixels[0].size()<<" pixels wide"<<endl;
+    }
+    else
+    {
+      cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+    }
+  }
+  else
+  {
+    cout<<"Please specify one image file!\n";
+  }
   return 0;
 }
